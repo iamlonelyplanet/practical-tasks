84841ec216e3a058c6e85084782d390c7e9cd9c8
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2026-01-19 12:40:06 +0500

    Lesson 2: done

--- src/com/walking/lesson2_operators_console_io/Task1.java
+++ src/com/walking/lesson2_operators_console_io/Task1.java
@@ -1,5 +1,7 @@
 package com.walking.lesson2_operators_console_io;
 
+import java.util.Scanner;
+
 /**
  * Введите с клавиатуры вещественные числа a и b. Выведите на экран результат выражения
  * c = b*a*(a + b)/(a2),         где a2 - a в квадрате
@@ -8,5 +10,27 @@ package com.walking.lesson2_operators_console_io;
  */
 public class Task1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите первое вещественное число: ");
+        double a =  sc.nextDouble();
+        System.out.println("Введите второе вещественное число: ");
+        double b =  sc.nextDouble();
+
+        double c = b * a * (a + b) / (a * a);
+        System.out.printf("Результат сложения вещественных чисел: %f \n", c);
+
+        System.out.println("Введите первое целое число: ");
+        int a1 =  sc.nextInt();
+        System.out.println("Введите второе целое число: ");
+        int b1 =  sc.nextInt();
+
+        int c1 = b1 * a1 * (a1 + b1) / (a1 * a1);
+        System.out.printf("Результат сложения целых чисел: %d", c1);
+        sc.close();
     }
 }
+
+//     При использовании int мы могли потерять дробную часть результата при делении.
+//     Также при выводе результата в консоль, используя переменную типа double, мы всегда видим дробную часть, даже
+//     если она пуста. Например: 1.0
--- src/com/walking/lesson2_operators_console_io/Task2.java
+++ src/com/walking/lesson2_operators_console_io/Task2.java
@@ -1,5 +1,7 @@
 package com.walking.lesson2_operators_console_io;
 
+import java.util.Scanner;
+
 /**
  * Введите с клавиатуры целые числа a и b.
  * Выведите на экран результат сравнения:
@@ -7,5 +9,21 @@ package com.walking.lesson2_operators_console_io;
  */
 public class Task2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое целое число: ");
+        int a = sc.nextInt();
+        System.out.println("Введите второе целое число: ");
+        int b = sc.nextInt();
+        sc.close();
+
+        boolean result = (a * a * a > b * b);
+        System.out.println(result);
+
+        if  (result) {
+            System.out.println("a в кубе больше, чем b в квадрате");
+        }
+        if (!result) {
+            System.out.println("a в кубе меньше, чем b в квадрате");
+        }
     }
 }
