321b72d04048f4fd084194c2daaf09e355f39271
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2026-01-25 10:36:36 +0500

    Lesson 7: all done

--- src/com/walking/lesson7_varargs_overloading/Task2.java
+++ src/com/walking/lesson7_varargs_overloading/Task2.java
@@ -18,5 +18,86 @@ package com.walking.lesson7_varargs_overloading;
  */
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(sum(1.2, 2.4, 3.0));
+        System.out.println(sum((byte)1, (byte)2, (byte)115));
+        System.out.println(sum('1', ' ', 'a'));
+        System.out.println(sum(1, 2, 3));
+        System.out.println(sum((short)1, (short)2, (short)3));
+        System.out.println(sum(true, false));
+        System.out.println(sum(1f, 2035.5f, 3f));
+        System.out.println(sum(1L, 2035L, 3L));
+    }
+
+    static int sum (int ...ints) {
+        int sum = 0;
+        for (int i : ints) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static short sum (short ...shorts) {
+        short sum = 0;
+        for (short i : shorts) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static long sum (long ...longs) {
+        long sum = 0;
+        for (long i : longs) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static double sum (double ...doubles) {
+        double sum = 0;
+        for (double i : doubles) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static byte sum (byte ...bytes) {
+        byte sum = 0;
+        for (byte i : bytes) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static float sum (float ...floats) {
+        float sum = 0;
+        for (float i : floats) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static String sum (char ...chars) {
+        String sum = "";
+        for (char i : chars) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    static boolean sum (boolean ...booleans) {
+        for (boolean i : booleans) {
+            if (!i) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
