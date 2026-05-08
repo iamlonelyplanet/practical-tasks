99cd09be7efebb1731da3f7a573a2f6bc4b3abd3
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2026-05-08 10:05:02 +0500

    Lesson 18-2: done

---
+++ src/com/walking/lesson18_instanceof_getClass/task1_getClass/model/Cat.java
@@ -0,0 +1,12 @@
+package com.walking.lesson18_instanceof_getClass.task1_getClass.model;
+
+public class Cat extends Animal {
+
+    public Cat() {
+        super("meow");
+    }
+
+    public void meow(){
+        sound();
+    }
+}
---
+++ src/com/walking/lesson18_instanceof_getClass/task1_getClass/model/Dog.java
@@ -0,0 +1,14 @@
+package com.walking.lesson18_instanceof_getClass.task1_getClass.model;
+
+public class Dog extends Animal {
+    //В родственных подклассах другой подход, без константы X_SOUND, вместо которой здесь String
+    public static final String DOG_SOUND = "woof";
+
+    public Dog() {
+        super(DOG_SOUND);
+    }
+
+    public void woof(){
+        sound();
+    }
+}
